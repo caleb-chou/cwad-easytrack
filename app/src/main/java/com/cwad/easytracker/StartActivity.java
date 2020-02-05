7984fdd97395e5070096de4ebd1c237b0cf570a7
caleb <haloisawesome412@gmail.com>
2020-02-05 11:56:28 -0600

    Added Permissions

--- app/src/main/java/com/cwad/easytracker/StartActivity.java
+++ app/src/main/java/com/cwad/easytracker/StartActivity.java
@@ -1,14 +1,60 @@
 package com.cwad.easytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.Manifest.permission.SEND_SMS;
 
 public class StartActivity extends AppCompatActivity {
+
+    String phone;
+    int SMS_PERMISSION_CODE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        SharedPreferences settings
+                = PreferenceManager.getDefaultSharedPreferences(StartActivity.this);
+        final SharedPreferences.Editor settings_editor = settings.edit();
+
+        final EditText pn = findViewById(R.id.phone_number);
+        Button save = findViewById(R.id.save_button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phone = pn.getText().toString();
+                settings_editor.putString("pn",phone);
+                settings_editor.apply();
+            }
+        });
+
+
+        if(ContextCompat.checkSelfPermission(
+                StartActivity.this,
+                SEND_SMS
+        ) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(StartActivity.this,
+                new String[]{Manifest.permission.SEND_SMS},
+                SMS_PERMISSION_CODE);
+        else
+            Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "Permission already granted. Awesome!",
+                    Snackbar.LENGTH_LONG
+            ).show();
     }
 }
